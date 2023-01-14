21f1077b6610c9bbb6ad9fe852cf915e3742c29e
Stephen Giovanni Saputra <stephen.gsaputra@gmail.com>
2023-01-14 11:06:30 +0700

    Initial commit

---
+++ lib/input_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(children: [
+          Expanded(
+              child: Row(children: [
+            Expanded(child: ReusableCard(color: Color(0xFF1D1E33))),
+            Expanded(child: ReusableCard(color: Color(0xFF1D1E33)))
+          ])),
+          Expanded(child: ReusableCard(color: Color(0xFF1D1E33))),
+          Expanded(
+              child: Row(children: [
+            Expanded(child: ReusableCard(color: Color(0xFF1D1E33))),
+            Expanded(child: ReusableCard(color: Color(0xFF1D1E33)))
+          ]))
+        ]));
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+  Color color;
+
+  ReusableCard({@required this.color});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(15),
+      ),
+    );
+  }
+}
